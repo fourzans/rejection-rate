832dfa24b14ad88a2b55348d2312c1944589fffe
fourzans <fourzans@gmail.com>
2020-07-05 12:27:26 -0500

    Adding Firebase Admin

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
 const app = express();
 
@@ -8,4 +10,9 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
+});
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://rejection-rate.firebaseio.com"
 });
